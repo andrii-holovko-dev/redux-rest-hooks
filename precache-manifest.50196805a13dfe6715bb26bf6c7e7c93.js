7a7676cee062c9512548eed3f9a3a9af69e68573
andrii <andrii@anodot.com>
2020-04-05 23:46:49 +0300

    Updates

---
+++ precache-manifest.50196805a13dfe6715bb26bf6c7e7c93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0167957a3c9f888fdd98ae2dd8279718",
+    "url": "/redux-rest-hooks/index.html"
+  },
+  {
+    "revision": "19be7b2dcb5d1ed6fd0c",
+    "url": "/redux-rest-hooks/static/js/2.780fc98a.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/redux-rest-hooks/static/js/2.780fc98a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6587a240b6dbe38f6f5e",
+    "url": "/redux-rest-hooks/static/js/main.8f0a9ba4.chunk.js"
+  },
+  {
+    "revision": "0dfcd0caef217280bf9c",
+    "url": "/redux-rest-hooks/static/js/runtime-main.d421e654.js"
+  }
+]);
